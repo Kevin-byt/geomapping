fe7503463d847ef444df91e06508cc9172afeb02
unknown <myapps255@gmail.com>
2022-03-10 17:01:07 -0600

    updates

--- geo.py
+++ geo.py
@@ -5,19 +5,16 @@ import pandas as pd
 
 file="data.xlsx"
 data = pd.read_excel(file)
-data.head()
+# data.head()
 
 center = [-0.0000000, 0.0000000]
 map = folium.Map(location=center, zoom_start=2)
+geolocator = Nominatim(user_agent="my_request")
 
 for index, data in data.iterrows():
-    address = data['Address']
-    geolocator = Nominatim(user_agent="my_request")
+    address = data['Address']    
     position = geolocator.geocode(address)
     location = [position.latitude, position.longitude]
     folium.Marker(location, popup = f'Name:{data["First Name"]}').add_to(map)
 
 map.save('index.html')
-
-# $ pip freeze > requirements.txt
-# $ pip install -r requirements.txt
